7bd0ad9e757552adf5820f74798a75e6f1a221b6
adisingh0824 <sadityasingh7990@gmail.com>
2026-04-10 22:01:43 +0530

    Commit

--- momgootest.js
+++ momgootest.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+
 // MongoDB Connection
 mongoose.connect("mongodb://127.0.0.1:27017/mydb")
     .then(() => console.log("DB Connected"))
